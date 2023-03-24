cb514fc3c41386c539daa1dc4e3141a0f01080dd
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-03-24 08:53:29 +0100

    +1

--- markdown2html.py
+++ markdown2html.py
@@ -6,7 +6,7 @@ from os.path import exists
 
 if __name__ == "__main__":
 
-    if len (argv) < 3:
+    if len(argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html")
         exit(1)
 
