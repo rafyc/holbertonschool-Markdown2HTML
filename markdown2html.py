7a65d84eef7d8baf3c6fb3472b6945f104907039
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-03-24 08:57:25 +0100

    +1

--- markdown2html.py
+++ markdown2html.py
@@ -7,11 +7,11 @@ from os.path import exists
 if __name__ == "__main__":
 
     if len(argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html")
+        print("Usage: ./markdown2html.py README.md README.html",file=stderr)
         exit(1)
 
     if not exists(argv[1]):
-        print(f"Missing {argv[1]}")
+        print(f"Missing {argv[1]}",file=stderr)
         exit(1)
 
     exit(0)
