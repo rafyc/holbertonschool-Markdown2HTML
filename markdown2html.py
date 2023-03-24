052ca9798bc9d10b05d3c880d969b2f347eec63c
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-03-24 15:36:28 +0100

    +3

--- markdown2html.py
+++ markdown2html.py
@@ -4,7 +4,8 @@ markdown2html.py - Convert a markdown file to HTML format.
 
 Usage: ./markdown2html.py input.md output.html
 
-This script reads a markdown file and writes an HTML file with the same content, but formatted as HTML.
+This script reads a markdown file and writes an HTML file with the same content
+but formatted as HTML.
 It supports headings, unordered lists, and paragraphs.
 """
 
@@ -22,22 +23,23 @@ if __name__ == "__main__":
         exit(1)
 
     with open(argv[1], 'r', encoding='utf-8') as file:
-    with open(argv[2], "w", encoding='utf-8') as html:
-        nextLine = file.readline()
-        while nextLine:
-            nbhash = nextLine.count("#")
-            unorderList = nextLine.strip("- ").rstrip()
-            if nbhash != 0:
-                line = nextLine.lstrip('# ').lstrip('/n')
-                html.write(f'<h{nbhash}>{line.rstrip()}</h{nbhash}>\n')
-            if '-' in nextLine:
-                html.write('<ul>\n')
-                html.write(f'<li>{unorderList}</li>\n')
-                nextLine = file.readline()
-                while nextLine.strip().startswith('-'):
-                    html.write(f'<li>{nextLine.strip("- ").rstrip()}</li>\n')
+        with open(argv[2], "w", encoding='utf-8') as html:
+            nextLine = file.readline()
+            while nextLine:
+                nbhash = nextLine.count("#")
+                unorderList = nextLine.strip("- ").rstrip()
+                if nbhash != 0:
+                    line = nextLine.lstrip('# ').lstrip('/n')
+                    html.write(f'<h{nbhash}>{line.rstrip()}</h{nbhash}>\n')
+                if '-' in nextLine:
+                    html.write('<ul>\n')
+                    html.write(f'<li>{unorderList}</li>\n')
                     nextLine = file.readline()
-                html.write('</ul>\n')
-            else:
-                nextLine = file.readline()
-    exit(0)
+                    while nextLine.strip().startswith('-'):
+                        html.write(f'<li>{nextLine.strip("- ").rstrip()}</'
+                                   f'li>\n')
+                        nextLine = file.readline()
+                    html.write('</ul>\n')
+                else:
+                    nextLine = file.readline()
+        exit(0)
