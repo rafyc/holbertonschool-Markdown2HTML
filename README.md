a953bcb891590d500a462b8b3ad8033866488e32
Raphaël Chemouni <rapha.chemouni@gmail.com>
2023-03-24 14:52:35 +0100

    +3

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 # My title3
 #### My title4
 ### My title5
+- Hello
+- Bye
--- markdown2html.py
+++ markdown2html.py
@@ -13,13 +13,23 @@ if __name__ == "__main__":
     if not exists(argv[1]):
         print(f"Missing {argv[1]}", file=stderr)
         exit(1)
-
-    with open(argv[1], 'r', encoding='utf-8') as file:
-        with open(argv[2], "w", encoding='utf-8') as html:
-            for line in file:
-                nbhash = line.count("#")
-                if (nbhash != 0):
-                    line = line.lstrip('# ').lstrip('/n')
-                    html.write(f'<h{nbhash}>{line.rstrip()}</h{nbhash}>\n')
-
+with open(argv[1], 'r', encoding='utf-8') as file:
+    with open(argv[2], "w", encoding='utf-8') as html:
+        nextLine = file.readline()
+        while nextLine:
+            nbhash = nextLine.count("#")
+            unorderList = nextLine.strip("- ").rstrip()
+            if nbhash != 0:
+                line = nextLine.lstrip('# ').lstrip('/n')
+                html.write(f'<h{nbhash}>{line.rstrip()}</h{nbhash}>\n')
+            if '-' in nextLine:
+                html.write('<ul>\n')
+                html.write(f'<li>{unorderList}</li>\n')
+                nextLine = file.readline()
+                while nextLine.strip().startswith('-'):
+                    html.write(f'<li>{nextLine.strip("- ").rstrip()}</li>\n')
+                    nextLine = file.readline()
+                html.write('</ul>\n')
+            else:
+                nextLine = file.readline()
     exit(0)
